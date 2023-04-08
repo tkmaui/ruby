368e71b6339f6baf52e04333deb79f22179b3bc4
EC2 Default User <ec2-user@ip-172-31-7-216.ap-northeast-1.compute.internal>
2023-04-08 13:43:07 +0000

    Add 9章

---
+++ amouts.rb
@@ -0,0 +1,4 @@
+amouts={"りんご"=>2,"いちご"=>5,"オレンジ"=>3}
+amouts.each do |fruit,amout|
+  puts "#{fruit}は#{amout}個です。"
+end
---
+++ input.rb
@@ -0,0 +1,4 @@
+puts "キーボードから何か入力してみましょう"
+input_key = gets
+gets "入力された内容は#{input_key}"
+
